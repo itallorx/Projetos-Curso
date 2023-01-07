da763590824c7eef25c787e47944b1efcb5be583
itallorx <79156690+itallorx@users.noreply.github.com>
2023-01-07 14:08:28 -0300

    Add files via upload

---
+++ projeto 3/app.py
@@ -0,0 +1,10 @@
+velocidade = int(input('Digite sua velocidade'))
+velocidade_maxima = 80
+if velocidade <= velocidade_maxima:
+ print('Não levou multa')
+elif velocidade > velocidade_maxima and velocidade <= velocidade_maxima + 10:
+    print('levou multa leve')
+elif velocidade >= velocidade_maxima +11 and velocidade <= velocidade_maxima +20:
+    print('Levou multa grave')
+elif velocidade > velocidade_maxima +20:
+    print('levou multa gravissima')
